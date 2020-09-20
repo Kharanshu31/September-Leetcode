e39ae3700edbf8528283f72e5e7d10c9016a3681
Kharanshu Dewan <63278591+Kharanshu31@users.noreply.github.com>
2020-09-20 13:54:08 +0530

    Day 9

---
+++ CompareVersionNumbers.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        int i=0,j=0;
+        while(i<version1.length() || j<version2.length())
+        {
+            int n1=0;
+            int n2=0;
+            while(i<version1.length() && version1[i]!='.')
+            {
+                n1=n1*10 + (version1[i++]-'0');
+            }
+            while(j<version2.length() && version2[j]!='.')
+            {
+                n2=n2*10 + (version2[j++]-'0');
+            }
+            
+            if(n1<n2)
+                return -1;
+            else if(n1>n2)
+                return 1;
+            
+            i++;
+            j++;
+        }
+        
+        return 0;
+    }
+};
